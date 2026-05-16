60d83a3316c1adefc65fdd3555f0897037c18a7b
Muhamad Ridwan <mrdwn1812@gmail.com>
2026-05-16 18:29:17 +0700

    update

--- resources/views/bookings/show.blade.php
+++ resources/views/bookings/show.blade.php
@@ -18,7 +18,7 @@
             <div class="absolute bottom-0 left-1/2 h-64 w-[120%] -translate-x-1/2 bg-gradient-to-t from-white to-transparent"></div>
         </div>
 
-        <div class="relative z-10 mx-auto max-w-3xl px-4 pb-16 pt-8 sm:px-6 lg:px-8">
+        <div class="relative z-10 mx-auto max-w-7xl px-4 pb-16 pt-8 sm:px-6 lg:px-8">
             <a href="{{ route('bookings.index') }}" class="mb-6 inline-flex items-center gap-2 text-sm font-semibold text-brand-700 transition hover:text-brand-800">
                 <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                     <path fill-rule="evenodd" d="M17 10a.75.75 0 01-.75.75H5.612l4.158 3.96a.75.75 0 11-1.04 1.08l-5.5-5.25a.75.75 0 010-1.08l5.5-5.25a.75.75 0 111.04 1.08L5.612 9.25H16.25A.75.75 0 0117 10z" clip-rule="evenodd" />
